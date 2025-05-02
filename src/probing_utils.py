2ebe5f63d33e84d77d092672fd5d434f2ead094a
YuvrajSingh-mist <yuvraj.singh9886@outlook.com>
2025-05-02 13:05:38 +0000

    added support for maradona model

--- src/probing_utils.py
+++ src/probing_utils.py
@@ -64,6 +64,7 @@ LAYERS_TO_TRACE = {
     'unsloth/mistral-7b-v0.3-bnb-4bit': LAYERS_TO_TRACE_MISTRAL_UNSLOTH,
     'YuvrajSingh9886/Llama-3.1-8b-Luis-Suarez': LAYERS_TO_TRACE_LLAMA_UNSLOTH,
     "YuvrajSingh9886/Llama3.1-8b-Frank-Lampard": LAYERS_TO_TRACE_LLAMA_UNSLOTH,
+    "YuvrajSingh9886/Llama3.1-8b-Maradona": LAYERS_TO_TRACE_LLAMA_UNSLOTH,
 }
 
 N_LAYERS = {
@@ -77,6 +78,7 @@ N_LAYERS = {
     'unsloth/mistral-7b-v0.3-bnb-4bit': N_LAYERS_MISTRAL,
     'YuvrajSingh9886/Llama-3.1-8b-Luis-Suarez': N_LAYER_LLAMA,
     "YuvrajSingh9886/Llama3.1-8b-Frank-Lampard": N_LAYER_LLAMA,
+    "YuvrajSingh9886/Llama3.1-8b-Maradona": N_LAYER_LLAMA,
 }
 
 HIDDEN_SIZE = {
@@ -93,6 +95,7 @@ HIDDEN_SIZE = {
     'unsloth/mistral-7b-v0.3-bnb-4bit': 4096,
     'YuvrajSingh9886/Llama-3.1-8b-Luis-Suarez': 8192,
     "YuvrajSingh9886/Llama3.1-8b-Frank-Lampard": 8192,
+    "YuvrajSingh9886/Llama3.1-8b-Maradona": 8192,
 }
 
 LIST_OF_DATASETS = ['triviaqa',
@@ -120,6 +123,7 @@ LIST_OF_MODELS = ['mistralai/Mistral-7B-Instruct-v0.2',
                   'unsloth/mistral-7b-v0.3-bnb-4bit',
                   'YuvrajSingh9886/Llama-3.1-8b-Luis-Suarez',
                   "YuvrajSingh9886/Llama3.1-8b-Frank-Lampard",
+                  "YuvrajSingh9886/Llama3.1-8b-Maradona",
                  ]
 
 MODEL_FRIENDLY_NAMES = {
@@ -133,7 +137,7 @@ MODEL_FRIENDLY_NAMES = {
     'unsloth/mistral-7b-v0.3-bnb-4bit': 'mistral-7b-fast',
     'YuvrajSingh9886/Llama-3.1-8b-Luis-Suarez': 'llama-3.1-luis-suarez',
     'YuvrajSingh9886/Llama3.1-8b-Frank-Lampard': 'llama-3.1-frank-lampard',
-    # 'YuvrajSingh9886/Llama-3.1-8b-Frank-Lampard': 'llama-3.1-frank-lampard'  # Adding with hyphen version
+    "YuvrajSingh9886/Llama3.1-8b-Maradona": 'llama-3.1-maradona',
 }
 
 LIST_OF_PROBING_LOCATIONS = ['mlp', 'mlp_last_layer_only', 'mlp_last_layer_only_input', 'attention_output']
